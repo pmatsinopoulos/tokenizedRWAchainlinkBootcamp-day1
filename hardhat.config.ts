b096a51cad4c79ae4e86bfe615f8df3f7719add5
Panos Matsinopoulos <panos@mixlr.com>
2024-10-01 11:07:22 +0300

    Solidity compiler 0.8.27, EVM Version 'paris' and optimizer is turned on with 200 runs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,16 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
+  solidity: {
+    version: "0.8.27",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      evmVersion: "paris",
+    },
+  },
 };
 
 export default config;
