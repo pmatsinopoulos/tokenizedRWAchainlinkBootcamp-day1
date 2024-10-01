90873735b5a58b8c597f049afe29837c4a31c8b7
Panos Matsinopoulos <panos@mixlr.com>
2024-10-01 10:46:43 +0300

    Withdraw.sol utility contract

---
+++ contracts/utils/Withdraw.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {OwnerIsCreator} from "@chainlink/contracts-ccip/src/v0.8/shared/access/OwnerIsCreator.sol";
+
+/**
+ * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED VALUES FOR CLARITY.
+ * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
+ * DO NOT USE THIS CODE IN PRODUCTION.
+ */
+
+// Panagiotis Matsinopoulos copied from https://cll-devrel.gitbook.io/tokenized-rwa-bootcamp-2024/day-1/exercise-1-cross-chain-real-estate#create-crosschainburnandminterc1155.sol
+// as part of the Tokenized RWA Bootcamp Oct 2024
+//
+contract Withdraw is OwnerIsCreator {
+    using SafeERC20 for IERC20;
+
+    error NothingToWithdraw();
+    error FailedToWithdrawEth(address owner, address target, uint256 value);
+
+    function withdraw(address _beneficiary) public onlyOwner {
+        uint256 amount = address(this).balance;
+
+        if (amount == 0) revert NothingToWithdraw();
+
+        (bool sent, ) = _beneficiary.call{value: amount}("");
+
+        if (!sent) revert FailedToWithdrawEth(msg.sender, _beneficiary, amount);
+    }
+
+    function withdrawToken(
+        address _beneficiary,
+        address _token
+    ) public onlyOwner {
+        uint256 amount = IERC20(_token).balanceOf(address(this));
+
+        if (amount == 0) revert NothingToWithdraw();
+
+        IERC20(_token).safeTransfer(_beneficiary, amount);
+    }
+}
